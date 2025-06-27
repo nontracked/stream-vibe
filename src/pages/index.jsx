83fb79c0575840b42e29ad5af715ae2e59d89e46
Murad Emi <murad2013408@gmail.com>
2025-06-27 16:44:15 +0500

    Созданы и подготовлены все файлы

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import { Head } from "minista"
+export default function () {
+  return (
+    <>
+      <Head>
+        <title>Hello!</title>
+      </Head>
+      <h1 className="container">Hello!</h1>
+    </>
+  )
+}
