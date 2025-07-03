d7433f409cd8934fd0a5be6af99398a7dff2a8c3
Murad Emi <murad2013408@gmail.com>
2025-07-03 16:51:07 +0500

    Header готов

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -3,6 +3,7 @@ import cn from 'classnames'
 import './header.scss'
 import Button from '@/components/Button'
 import { Bell, Bookmark, Search } from 'lucide-react';
+import BurgerButton from '@/components/BurgerButton';
 export const Header = ({ url }) => {
   const navItems = [
     {
@@ -23,25 +24,31 @@ export const Header = ({ url }) => {
     },
   ]
   return (
-    <header className="header">
+    <header className="header" data-js-overlay-menu="">
       <div className="header__inner container">
         <Logo className="header__logo" loading="eager" />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {navItems.map(({ title, href }, index) => (
-              <li className="header__menu-item" key={index}>
-                <a className={cn("header__menu-link", {
-                  "is-active": href === url
-                })} href={href}>{title}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="header__actions">
-          <Button tranparent isLabelVusible={false} hasIcon icon={<Search className='header__icon' />} className="header__button" label={"Search"} />
-          <Button tranparent isLabelVusible={false} hasIcon icon={<Bell className='header__icon' />} className="header__button" label={"Search"} />
-        </div>
-
+        <dialog className="header__overlay-menu-dialog"
+          data-js-overlay-menu-dialog="">
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {navItems.map(({ title, href }, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a className={cn("header__menu-link", {
+                    "is-active": href === url
+                  })} href={href}>{title}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button tranparent isLabelVusible={false} hasIcon icon={<Search className='header__icon' />} className="header__button" label={"Search"} />
+            <Button tranparent isLabelVusible={false} hasIcon icon={<Bell className='header__icon' />} className="header__button" label={"Search"} />
+          </div>
+        </dialog>
+        <BurgerButton classname="header__burger-button visible-tablet"
+          extraAttrs={{
+            'data-js-overlay-menu-burger-button': ''
+          }} />
       </div>
     </header >
   )
